b0f34e6ab6642bee0a2c0f48b813d85cf86e63d7
pearpearB <smilyoung@gmail.com>
2022-03-24 20:25:37 +0900

    norm check finish

--- ft_printf.h
+++ ft_printf.h
@@ -6,25 +6,25 @@
 /*   By: jabae <jabae@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 13:26:03 by jabae             #+#    #+#             */
-/*   Updated: 2022/03/24 18:17:54 by jabae            ###   ########.fr       */
+/*   Updated: 2022/03/24 20:23:23 by jabae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdarg.h>
+# include <unistd.h>
+# include <stdarg.h>
 
-int	ft_printf(const char *format, ...);
-int	ft_switch_format(va_list ap, const char *format, int *idx);
+int		ft_printf(const char *format, ...);
+int		ft_switch_format(va_list ap, const char *format, int *idx);
 
 size_t	ft_strlen(const char *str);
 void	ft_putnbr_fd(long long num, int fd);
 char	*ft_make_hex(char c);
 
-int		ft_putstr_count(char *str);
 int		ft_putchar_count(char c);
+int		ft_putstr_count(char *str);
 int		ft_putint_count(long long num);
 int		ft_putunsignedint_count(unsigned int num);
 int		ft_puthex_count(unsigned int num, char *hexbase);
